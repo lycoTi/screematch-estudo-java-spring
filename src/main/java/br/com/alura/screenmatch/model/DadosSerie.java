bc6ba76b5e70f8cbaf9f531195caab87106107f3
tiago <tiagosantos__@live.com>
2024-02-28 13:58:02 -0400

    sd

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -8,4 +8,4 @@ public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
                          @JsonAlias("imdbRating") String avaliacao) {
 }
-ds
+dsdsd
